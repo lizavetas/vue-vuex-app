f1c1a2785eaea17b8fb3ec66a764ddee8fc6bcb9
Elizaveta Shcherbakova <e.shcherbakova@lizaveta.de>
2017-07-24 17:10:15 +0200

    first

---
+++ src/components/Carfinder.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        carfinder
+        <test></test>
+    </div>
+</template>
+
+<script>
+    import test from './test';
+    export default {
+        name: 'carfinder',
+        components: {
+            test
+        }
+    }
+</script>
