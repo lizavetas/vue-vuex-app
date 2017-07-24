1d6a1edfac3abe696d3d210e87914337e6a956a9
Elizaveta Shcherbakova <e.shcherbakova@lizaveta.de>
2017-07-24 17:47:30 +0200

    mutations

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,25 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
                 <app-result></app-result>
-                <hr>
                 <app-result-b></app-result-b>
+                <hr>
                 <app-counter></app-counter>
+                <app-counter-b></app-counter-b>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Counter from './components/Counter.vue';
+    import Counter from './components/Counter';
+    import CounterB from './components/CouneterB';
     import Result from './components/Result.vue';
     import ResultB from './components/ResultB.vue';
 
     export default {
         components: {
             appCounter: Counter,
+            appCounterB: CounterB,
             appResult: Result,
             appResultB: ResultB,
         }
