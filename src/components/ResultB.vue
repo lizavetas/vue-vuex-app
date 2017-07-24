c5dde617305dd46a7257925ec636b84ef1f12088
Elizaveta Shcherbakova <e.shcherbakova@lizaveta.de>
2017-07-24 17:35:46 +0200

    mapGetters

--- src/components/ResultB.vue
+++ src/components/ResultB.vue
@@ -1,13 +1,30 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of Clicks: {{ stringCounter }}</p>
+    </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+
     export default {
         computed: {
-            counter() {
-                return this.$store.getters.doubleCounter;
-            }
+            ...mapGetters([
+                'doubleCounter',
+                'stringCounter'
+            ])
         }
+
+/*        computed: mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ])*/
+//        counter() {
+//            return this.$store.getters.doubleCounter;
+//        },
+//        clicks() {
+//            return this.$store.getters.stringCounter;
+//        }
     }
 </script>
