fc857b6fe44acf25b51df0e5da1d327190bcef4a
Elizaveta Shcherbakova <e.shcherbakova@lizaveta.de>
2017-07-24 18:36:42 +0200

    mapActions

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
+
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    plugins: [createLogger()],
     state: {
         counter: 0
     },
@@ -22,6 +25,20 @@ export const store = new Vuex.Store({
         decrement: state => {
             state.counter--;
         }
+    },
+    actions: {
+        increment: ({commit}) => {
+            commit('increment');
+        },
+        decrement: ({commit}) => {
+            commit('decrement');
+        },
+        asynIncrement: ({comit}) => {
+            setTimeout(() => {
+                commit('increment');
+            }, 1000)
+        }
+
     }
 });
 
